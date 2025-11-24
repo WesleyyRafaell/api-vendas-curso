c46e839100598e21da9bef784e4b4479ee016161
Wesley Rafael <wesley.santos@opah.com>
2025-11-24 18:02:45 -0300

    fix: initial structure

---
+++ src/common/infrastructure/http/server.ts
@@ -0,0 +1,7 @@
+import { env } from '../env'
+import { app } from './app'
+
+app.listen(env.PORT, () => {
+  console.log(`Server running on port ${env.PORT}! 🏆`)
+  console.log('API docs available at GET /docs 📚')
+})
