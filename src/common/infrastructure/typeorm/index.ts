1b59800f84098180aa0b6f2d95a87d3e6f3de222
Wesley Rafael <wesley.santos@opah.com>
2025-12-16 22:43:06 -0300

    feat: create products domain

--- src/common/infrastructure/typeorm/index.ts
+++ src/common/infrastructure/typeorm/index.ts
@@ -4,24 +4,14 @@ import { env } from '../env'
 
 export const dataSource = new DataSource({
   type: env.DB_TYPE,
-
   host: env.DB_HOST,
-
   port: env.DB_PORT,
-
   schema: env.DB_SCHEMA,
-
   database: env.DB_NAME,
-
   username: env.DB_USER,
-
   password: env.DB_PASS,
-
   entities: ['**/entities/**/*.ts'],
-
   migrations: ['**/migrations/**/*.ts'],
-
   synchronize: false,
-
   logging: false,
 })
